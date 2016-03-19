aa6c5288fea1cff2ebc1d4365f1f309315945b54
Eric Skram <eric@skram.com>
2016-03-19 13:39:53 -0700

    Finish thumbnails for Pro Referral and Registration projects.

--- scripts/postcss.config.js
+++ scripts/postcss.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    "use": ["precss", "postcss-easings", "postcss-import", "rucksack-css", "lost", "autoprefixer", "cssnano"],
+    "use": ["precss", "postcss-animation", "postcss-import", "rucksack-css", "postcss-size", "lost", "autoprefixer", "cssnano"],
 };
