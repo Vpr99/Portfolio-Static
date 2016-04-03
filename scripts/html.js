8d20f9af3784b5c2d7988fda3148389cbcd7ab83
Eric Skram <eric@skram.com>
2016-04-03 14:55:32 -0700

    Posthtml
    * Convert HTML Build to PostHTML
    
    * Add includes to PostHTML
    
    * Add HTML Hinting to PostHTML.

---
+++ scripts/html.js
@@ -0,0 +1,54 @@
+var glob = require("glob-all"),
+    path = require("path"),
+    fs = require("fs-extra"),
+    permalinks = require("permalinks"),
+    posthtml = require("posthtml");
+/* Destination */
+var dest = "./build/";
+
+/* Source */
+var files = glob.sync([
+    __dirname + "/../**/*.html",
+    "!" + __dirname + "/../node_modules/**/*.html",
+    "!" + __dirname + "/../lib/components/**/*.html",
+    "!" + __dirname + "/../build/**/*.html"
+]);
+
+/* Loop through source files */
+files.forEach(function(file) {
+    var permalink;
+    var isIndex;
+    var ext = path.extname(file);
+    var basename = path.basename(file, ext);
+    var html = fs.readFileSync(file, 'utf-8');
+
+    /* Create Pretty Permalinks */
+    if(basename == "index") {
+        isIndex = true;
+        permalink = "index.html";
+    }
+    else {
+        var permalink = permalinks(':basename/index:ext', {
+            basename: basename,
+            ext: ext
+        });
+    }
+
+    console.log("");
+    console.log("===============================================================================");
+    console.log("Building " + basename + "...");
+
+    /* Configure PostHTML and run Mutations */
+
+    var hint = require('posthtml-hint')();
+
+    posthtml([ hint ])
+    .use(require('posthtml-include')({ encoding: 'utf-8' }))
+    .process(html)
+    .then(function(result) {
+        if (!fs.existsSync(dest + basename) && !isIndex) {
+            fs.mkdirSync(dest + basename);
+        }
+        fs.writeFile(dest + permalink, result.html, 'utf-8');
+    });
+});
